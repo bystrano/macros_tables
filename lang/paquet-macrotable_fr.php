23b7e7bce4c87360d7ea62500fd65e3b65d0fc0c
Michel Bystranowski <bystrano@gmx.ch>
2013-09-14 18:18:11 +0200

    commit initial - généré par la Fabrique

---
+++ lang/paquet-macrotable_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'macrotable_description' => 'description ici',
+	'macrotable_nom' => 'Macro Table',
+	'macrotable_slogan' => 'Une macro pour générer des tables d\'objets',
+);
+
+?>
