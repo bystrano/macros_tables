60fe419c91c838c52bd442037420ee510852b8bb
Michel Bystranowski <bystrano@gmx.ch>
2014-07-26 13:59:24 +0200

    ajoute le filtrage en ajax au filtre checkbox, avec option autosubmit

--- filtres/checkboxes.html.php
+++ filtres/checkboxes.html.php
@@ -7,3 +7,38 @@
 
   </form>
 </div>
+
+<?php if ($ajax === 'oui'): ?>
+<script type="text/javascript">
+  $(function () {
+      $('#filtre_<?php echo $nom_filtre; ?>')
+          .unbind('submit')
+          .submit(function (e) {
+              e.preventDefault();
+              <?php echo $nom; ?> = [];
+              $(this)
+                 .find('input[name="<?php echo $nom; ?>[]"]')
+                 .each(function (i, el) {
+                     if ($(el).attr('checked') === 'checked') {
+                         <?php echo $nom; ?>.push(el.value);
+                     }
+                 });
+              ajaxReload('<?php echo $nom_ajax; ?>', {
+                  args:{
+                      '<?php echo $nom; ?>': <?php echo $nom; ?>
+                  },
+                  history: true
+              });
+          })
+    <?php if ($autosubmit === 'oui'): ?>
+          .find('input')
+          .change(function () {
+              $('#filtre_<?php echo $nom_filtre; ?>').submit();
+          })
+          .parents('form').first()
+          .find('input[type="submit"]').hide();
+    <?php endif; ?>
+      ;
+  });
+</script>
+<?php endif; /*endif $ajax */ ?>
