fd9a9d9fa1a72e96f6df348a0ec42bc08baf9370
Michel Bystranowski <bystrano@gmx.ch>
2014-07-27 17:38:04 +0200

    corrige bug dans le cas où l'entete n'est pas définie

--- colonnes/champ_entete.php
+++ colonnes/champ_entete.php
@@ -6,15 +6,17 @@
      [(#ENV{tri_<?php echo $nom_table; ?>}|=={<?php echo $nom; ?>}|oui)
        [(#ENV{sens_<?php echo $nom_table; ?>}|=={-1}|?{
          <span class="croissant">
-           [(#TRI{>,<?php echo $entete . ($ajax ? ',ajax' : '');?>})]
+           [(#TRI{><?php echo ($entete ? ','.$entete : ',\'\'') .
+                               ($ajax ? ',ajax' : '');?>})]
          </span>,
          <span class="decroissant">
-           [(#TRI{<,<?php echo $entete . ($ajax ? ',ajax' : ''); ?>})]
+           [(#TRI{<<?php echo ($entete ? ','.$entete : ',\'\'') .
+                               ($ajax ? ',ajax' : ''); ?>})]
          </span>
        })]
      ]
      [(#ENV{tri_<?php echo $nom_table; ?>}|=={<?php echo $nom; ?>}|non)
-       [(#TRI{<?php echo $nom; ?>,<?php echo $entete . ($ajax ? ',ajax' : '');?>})]
+       [(#TRI{<?php echo $nom; ?><?php echo ($entete ? ','.$entete : ',\'\'') . ($ajax ? ',ajax' : '');?>})]
      ]<?php
    endif; ?>
 </th>
